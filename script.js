0f9b6dff7e734ae48c254fe55f80a510c9ccf333
Abdulkhafid001 <kabiruabdulkhafid@gmail.com>
2025-07-18 17:20:10 +0100

    all pages added waiting for nav to be fixed

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function myFunction() {
-  var navLinks = document.getElementById("navLinks");
-  if (navLinks.style.display === "block") {
-    navLinks.style.display = "none";
-  } else {
-    navLinks.style.display = "block";
-  }
+let menu = document.querySelector('#menu-btn');
+let navbar = document.querySelector('.navbar');
+
+menu.onclick = () => {
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+
+window.onscroll = () => {
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
 }
